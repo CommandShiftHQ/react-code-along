397c280a680f9c7e73244248b5e38dd82193bdbe
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2019-11-23 12:20:33 +0000

    Add onSubmit event and handling function

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Form = () => {
+  const handleOnSubmit = event => {
+    event.preventDefault();
+    const inputValue = event.target.name.value;
+    console.log('click click', inputValue);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
       <label htmlFor="name">
         Name:
         <input id="name" type="text" />
