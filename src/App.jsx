914da2acae21e606ad98422bba9c346a2a574ef1
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2019-11-23 12:14:24 +0000

    Add draft markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <h1>I am a title</h1>
+      <form>
+        <label htmlFor="name">
+          Name:
+          <input id="name" type="text" />
+        </label>
+        <button type="submit">Click me!</button>
+      </form>
+      <br />
+      <br />
+      <br />
+      <br />
+      <ul>
+        <li>Name 1</li>
+        <li>Name 2</li>
+        <li>Name 3</li>
+      </ul>
+    </>
+  );
 };
 
 export default App;
